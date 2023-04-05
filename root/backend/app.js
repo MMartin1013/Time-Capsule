78c0904b21a05bb515b42102ecc6ae9593e60cb9
Manolo Martin <martinmano38@gmail.com>
2023-04-05 17:46:42 -0400

    Integrated firebase

--- root/backend/app.js
+++ root/backend/app.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const { initializeApp } = require('firebase/app');
+const { getFirestore } = require('firebase/firestore');
+const firebaseConfig = require('./config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,5 +41,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//connecting firebase
+const fireApp = initializeApp(firebaseConfig);
+const database = getFirestore(fireApp);
 
 module.exports = app;
