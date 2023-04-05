36a8cc170191eaf8d8c5e5ed877a0f671dbf6c31
Brian Smith <MAC@Brians-MacBook-Air.local>
2023-04-05 12:10:15 -0400

    Render homepage in app

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,16 +1,17 @@
 import logo from "./logo.svg";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 import "./css/App.css";
 
 const router = createBrowserRouter([
   {
     /* Home page route where we will return hompage component */
     path: "/",
-    element: <div>Homepage</div>,
+    element: <Homepage/>,
   },
   {
     path: "/home",
-    element: <div>Homepage</div>,
+    element: <Homepage/>,
   },
 ]);
 
