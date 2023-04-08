58cb6dddcc354d5803c0743eeeecc5ae72ea4f02
Manolo Martin <martinmano38@gmail.com>
2023-04-08 18:06:41 -0400

    Skeletons for login endpoints

--- root/backend/routes/users.js
+++ root/backend/routes/users.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).send(`Reached ${req.originalUrl} with request: ${req.body} `); 
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:user', (req, res, next) => {
+  res.status(200).send(`Reached ${req.originalUrl} with request: ${req.body} `); 
 });
 
 module.exports = router;
