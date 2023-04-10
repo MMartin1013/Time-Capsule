a4a1055200848f399ceea01d855d40a425f7cc93
Manolo Martin <77557341+MMartin1013@users.noreply.github.com>
2023-04-10 12:58:56 -0400

    Create login endpoint (#3)
    * Skeletons for login endpoints
    
    * Get user endpoint checks if user exists
    
    * Can now post users
    
    * Remove useless endpoint and quickfix

--- root/backend/routes/users.js
+++ root/backend/routes/users.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const database = require('../database');
+const { collection, doc, getDoc, setDoc } = require('firebase/firestore');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET user and check if password matches */
+router.get('/:username/:password/', async (req, res, next) => {
+  const {username, password} = req.params;
+  const user = await getUser(database, username);
+  
+  if(user && user.password === password) {
+    res.status(200).send('User found!');
+  }else {
+    res.status(404).send('User not found.');
+  }
 });
+
+
+/* POST user and password if user does not exist*/
+router.post('/:username/:password/', async (req, res, next) => {
+  const {username, password} = req.params;
+  const user = await getUser(database, username);
+  
+  if(user) {
+    res.status(400).send('Username already exists');
+  }else {
+    try {
+      await setDoc(doc(database, 'users', username), {
+        username: username,
+        password: password,
+      });
+    }catch(e) {
+      res.status(400).send('Could not communicate with database');
+    }
+    
+    res.status(200).send('User has been added');
+  }
+});
+
+
+/* Retrieves user from the database */
+const getUser = async (db, username) => {
+  const usersRef = collection(db, 'users');
+  const userDoc = doc(usersRef, username);
+  const userSnapshot = await getDoc(userDoc);
+  const user = userSnapshot.data();
+    
+  return user;
+}
 
 module.exports = router;
