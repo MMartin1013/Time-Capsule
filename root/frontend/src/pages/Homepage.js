4932c6d8807c17e65e904d165e7f32e5518add56
Gynnie9x <112880888+Gynnie9x@users.noreply.github.com>
2023-04-19 23:41:31 -0400

    Basic home page

--- root/frontend/src/pages/Homepage.js
+++ root/frontend/src/pages/Homepage.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import logo from '../../src/logoTC.png';
+import axios from 'axios';
 
-export const Homepage = () => {
-    return (
-        <div>
-            Hello World!
+function HomePage() {
+  const [messageCount, setMessageCount] = useState(0);
+
+  useEffect(() => {
+    // fetch the count of messages from the backend and update the state
+    axios.get('http://localhost:3001/messages/count')
+      .then((response) => {
+        setMessageCount(response.data.count);
+      })
+      .catch((error) => {
+        console.error('Failed to fetch message count:', error);
+      });
+  }, []);
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ width: '400px', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+          <img src={logo} alt="Logo" />
         </div>
-    );
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <h2 style={{ marginBottom: '10px' }}>Inbox</h2>
+          {messageCount === 0 && <p>You have 0 message</p>}
+          {messageCount === 1 && <p>You have 1 message</p>}
+          {messageCount > 1 && <p>You have {messageCount} messages</p>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-export default Homepage;
+export default HomePage;
