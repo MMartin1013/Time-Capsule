c297e3e353b3511545edb5640646650e6e4cd2f0
Brian Smith <MAC@brians-air.myfiosgateway.com>
2023-04-20 11:22:58 -0400

    Login updated and reformatted. I changed the entry point to be the login with a splash sequence.

--- root/frontend/src/pages/Loginpage.js
+++ root/frontend/src/pages/Loginpage.js
@@ -1,8 +1,42 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import logo from '../../src/logoTC.png';
+import logo1 from '../../src/Time-Capsule-Clock-Logo.png';
+import logo2 from '../../src/logoTC.png'
 import axios from 'axios';
+import Splashr from 'splashr'
+
+const splash1 = <div class= "splash-screen1">
+  <div className = 'Logo' 
+  style={{ display: 'grid', 
+          justifyContent: 'center', 
+          alignItems: 'center',
+          width: '100%', 
+          height: '100%',
+          position: 'absolute',
+          backgroundColor: "#8eecec" }}
+    >
+    
+    <img style={{paddingBottom: 115}}src={logo1} alt="Logo" />
+            
+  </div>
+</div>
+
+const splash2 = <div class= "splash-screen2">
+  <div className = 'Logo' 
+  style={{ display: 'grid', 
+          justifyContent: 'center', 
+          width: '100%', 
+          height: '100%',
+          
+          backgroundColor: "#8eecec" }}
+    >
+    <h1 style={{ fontSize: 30 , fontWeight: "bold" , paddingTop: 175, fontStyle: 'italic'}}>"Connect To A Future You"</h1>
+    <img style={{paddingBottom: 695, paddingLeft: 110}}src={logo1} alt="Logo" />
+            
+  </div>
+</div>
+
 
 function LoginPage() {
   const [username, setUsername] = useState('');
@@ -42,27 +76,29 @@ function LoginPage() {
   };
   
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ width: '400px', justifyContent: 'center', alignItems: 'center' }}>
-        <div className = 'Logo' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
-          <img src={logo} alt="Logo" />
+    <Splashr splash={splash1} transitionTime={10000}>
+      
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',  backgroundColor: "#8eecec" }}>
+      <div style={{ alignItems: 'center' }}>
+      <Splashr splash={splash2} minDelay={7000} transitionTime={2500}>
+      <h1 style={{ paddingLeft: 300,width: 800, fontSize: 80 ,fontWeight: "bold" }}>Time Capsule</h1>
+      </Splashr>
+        <div className = 'Logo' style={{ zIndex: 5, display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+          <img style={{paddingBottom: 40}}src={logo2} alt="Logo" />
         </div>
-        <h1 style={{ textAlign: 'center', fontFamily: 'Lobster Two', fontStyle: 'italic' }}>Log In</h1>
+        <h1 style={{ textAlign: 'center', fontFamily: 'Lobster two' }}>Log In</h1>
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <label style={{ marginBottom: '10px' }}>
-            Username:
-          </label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} style={{ marginBottom: '10px' }} />
-          <label style={{ marginBottom: '10px' }}>
-            Password:
-          </label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginBottom: '10px' }} />
+          <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} style={{ borderRadius: 5,fontSize:20, textAlign: 'center', height: 30, width: 250,marginBottom: '10px' }} />
+          <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} style={{ borderRadius: 5,fontSize:20, textAlign: 'center', height: 30, width: 250, marginBottom: '10px' }} />
           <br />
-          <button onClick={handleLogin} style={{ fontWeight: 'bold', marginTop: '10px' }}>Log In</button>
+          <button onClick={handleLogin} style={{fontSize: 15,borderRadius: 5, height: 30, width: 100, fontWeight: 'bold' }}>Submit</button>
         </div>
-        <p style={{ textAlign: 'center', marginTop: '10px' }}><Link to="/register" style={{ textDecoration: 'underline', color: 'blue' }}>Create Account</Link></p>
+        <p style={{ textAlign: 'center', marginTop: '20px' }}>Don't have an account?  <Link to="/register" style={{ textDecoration: 'underline', color: 'blue' }}>Create Account</Link></p>
+        
       </div>
     </div>
+    </Splashr>
+    
   );
 }
 
