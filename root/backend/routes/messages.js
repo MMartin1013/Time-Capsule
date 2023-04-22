c2b66e05e5fc9361e5c56ae7d05f9d3a21d3bd33
Manolo Martin <martinmano38@gmail.com>
2023-04-22 12:24:52 -0400

    Correct logic for post message endpoint

--- root/backend/routes/messages.js
+++ root/backend/routes/messages.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const database = require('../database');
+const { addDoc, Timestamp } = require('firebase/firestore');
+const { getUser, getMessageCollection } = require('../utils/firebaseUtils');
 
 /* Get all messages from a user */
 router.get('/', async (req, res, next) => {
-   res.status(200);
+  res.status(200);
 });
 
-/* POST a new user message */
-router.post('/', async (req, res, next) => {
-   res.status(200);
-});
+/* POST a user message */
+router.post('/:username', async (req, res, next) => {
+    const {username} = req.params;
+    const {title, text, date} = req.body;
+    const user = await getUser(database, username);
+    
+    if(!user) {
+      res.status(400).send('User does not exist');
+    }else {
+      try {
+        const messagesRef = getMessageCollection(database, username);
+        if(!messagesRef) {
+            res.status(400).send('Could not communicate with database');
+        }
+
+        const temp_date = new Date(Date.parse(date));
+        const deliver_date = Timestamp.fromDate(temp_date);
+
+        if(!deliver_date) {
+          res.status(400).send('Wrong date format');
+        }
+
+        const message = {
+          title: title,
+          text: text,
+          deliver_date: deliver_date
+        }
+
+        await addDoc(messagesRef, message);
+      }catch(e) {
+        res.status(400).send('Could not communicate with database');
+      }
+      
+      res.status(200).send('Message has been added');
+    }
+  });
 
 module.exports = router;
--- root/backend/utils/firebaseUtils.js
+++ root/backend/utils/firebaseUtils.js
@@ -2,6 +2,10 @@ const { collection, doc, getDoc } = require('firebase/firestore');
 
 /* Retrieves user from the database */
 const getUser = async (db, username) => {
+    if(!username) {
+        return undefined;
+    }
+
     const usersRef = collection(db, 'users');
     const userDoc = doc(usersRef, username);
     let user;
@@ -10,9 +14,16 @@ const getUser = async (db, username) => {
         user = userSnapshot.data();
     } catch (error) {
         console.log(error);
-    } finally {
         return user;
     }
+    return user;
 }
 
-module.exports = { getUser };
+const getMessageCollection = (db, username) => {
+    const usersRef = collection(db, 'users');
+    const userRef = doc(usersRef, username);
+    const messagesRef = collection(userRef, 'messages');
+    return messagesRef;
+}
+
+module.exports = { getUser, getMessageCollection };
