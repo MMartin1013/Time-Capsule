ec4ccfd14c3a0f05c1c921153f9cb325f1a9411c
Manolo Martin <martinmano38@gmail.com>
2023-04-22 19:51:37 -0400

    Fix import issue

--- root/frontend/src/pages/Inboxpage.js
+++ root/frontend/src/pages/Inboxpage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import {useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import logo2 from '../../src/logoTC.png'
 import axios from 'axios';
