5b1b08f3e247405071b6718c20c65f5accfffebf
Manolo Martin <77557341+MMartin1013@users.noreply.github.com>
2023-04-23 17:26:11 -0400

    Finish inbox page (#12)
    * Added route params to frontend page urls
    
    * Change homepage to inbox page & shows base message
    
    * Fix import issue
    
    * Message buttons render without popup
    
    * Displays message when clicked

--- root/frontend/src/pages/Inboxpage.js
+++ root/frontend/src/pages/Inboxpage.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import logo2 from '../../src/logoTC.png'
 import axios from 'axios';
 import Splashr from 'splashr';
 import logo1 from '../../src/Time-Capsule-Gif.gif';
+import Message from '../components/Message';
 
 const splash1 = (
 <div class= "splash-screen1">
@@ -25,42 +26,40 @@ const splash1 = (
 
 
 
-function HomePage() {
-  const [password, setPassword] = useState('');
+function InboxPage() {
+  const [messages, setMessages] = useState([]);
   const {username} = useParams();
 
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
+  const requestMessages = () => {
     // make HTTP request to server to check login credentials
-      const data = JSON.stringify({
-        "username": username,
-        "password": password
-      });
-    
     const config = {
-      method: 'post',
+      method: 'get',
       maxBodyLength: Infinity,
-      url: 'http://localhost:3001/users/',
+      url: `http://localhost:3001/messages/${username}`,
       headers: {
       'Content-Type': 'application/json'
       },
-      data : data
     };
 
     axios.request(config)
       .then((response) => {
-        console.log('Login successful');
-        console.log(JSON.stringify(response.data));
-        // redirect user to home page or dashboard on successful login
-        navigate(`${username}/home`);
+        setMessages(response.data);
       })
       .catch((error) => {
       //display error message to user
-      console.error('Login failed', error);
-      console.log(error);
+      console.error('Could not retrieve messages', error);
       });
   };
+
+  useEffect(() => {
+    requestMessages();
+    const intervalId = setInterval(() => {  //assign interval to a variable to clear it.
+      requestMessages();
+    }, 10000)
+  
+    return () => clearInterval(intervalId); //This is important
+   
+  }, [])
   
   return (
     <Splashr splash={splash1} transitionTime={100} minDelay={750}>
@@ -72,20 +71,10 @@ function HomePage() {
           <img style={{paddingBottom: 40}}src={logo2} alt="Logo" />
         </div>
         <h1 style={{ textAlign: 'center', fontFamily: 'Lobster two' }}>Message Inbox</h1>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <textarea type="text" placeholder='Inbox is empty...'
-                  placeholderTextColor="#aaaaaa" rows="25" style={{ marginBottom: '10px' ,height: 200, width: 400,
-        borderRadius: 5,
-        overflow: 'hidden',
-        backgroundColor: 'white',
-        marginTop: 15,
-        marginBottom: 5,
-        marginLeft: 30,
-        marginRight: 30,
-        fontSize: '20px',
-        
-        
-       }} />
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around' }}>
+        {messages && messages.map(message => 
+              <Message title={message.title} text={message.text}/>
+              )}
         </div>
         <p style={{paddingBottom: 55, fontWeight: 'bold', textAlign: 'center', marginTop: '20px' }}>Wanna Create A Message?  <Link to={`/${username}/message`} style={{ fontWeight: 'bold', textDecoration: 'underline', color: 'blue' }}>Create Message</Link></p>
         
@@ -96,4 +85,4 @@ function HomePage() {
   );
 }
 
-export default HomePage;
+export default InboxPage;
